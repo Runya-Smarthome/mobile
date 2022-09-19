bbd6adbf93c58042c0648e66ea59b5b5695b33db
GifinoThoriq <gifino92@gmail.com>
2022-09-19 15:56:27 +0700

    [fix]fix avatar component

--- components/UI/Avatar.js
+++ components/UI/Avatar.js
@@ -1,31 +1,40 @@
-import { useEffect } from 'react'
 import { View, Image, Text, StyleSheet } from 'react-native'
 
 export default function Avatar({name, color, owner}) {
 
-    let image
+    const displayName =   name.charAt(0).toUpperCase() + name.slice(1)
+    
+    let image;
 
     if(color === "yellow"){
         image = <Image 
-                        style={{width: 100, height: 100}}
-                        source={require('../../assets/avatars/avatar-yellow.png')}
-                    />
+                    style={{width: 100, height: 100}}
+                    source={require('../../assets/avatars/avatar-yellow.png')}
+                />
     }else if(color === "blue"){
         image = <Image 
-        style={{width: 100, height: 100}}
-        source={require('../../assets/avatars/avatar-blue.png')}
-        />
+            style={{width: 100, height: 100}}
+            source={require('../../assets/avatars/avatar-blue.png')}
+            />
     }else if(color === "red"){
         image = <Image 
-        style={{width: 100, height: 100}}
-        source={require('../../assets/avatars/avatar-red.png')}
-        />
+            style={{width: 100, height: 100}}
+            source={require('../../assets/avatars/avatar-red.png')}
+            />
+    }else if(color === "green"){
+        image = <Image 
+            style={{width: 100, height: 100}}
+            source={require('../../assets/avatars/avatar-green.png')}
+            />
     }
-
+    
     return(
         <View style={styles.container}>
             {image}
-            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.name}>{displayName}</Text>
+            {
+                owner === "owner" ? <Text>(Owner)</Text> : null
+            }
         </View>
     )
 }
